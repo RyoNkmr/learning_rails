b8e1f42034db6a371e3c14ea8ecb6cfd91e3fc01
RyoNkmr <ryonakamuraryo@gmail.com>
2019-03-17 02:45:13 +0900

    2.3.3 user has_many microposts

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
-  validates :content, length: { maximum: 10 }
+  belongs_to :user
+  validates :content, length: { maximum: 10 },
+            presence: true
 end
